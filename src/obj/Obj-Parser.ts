db62cb2f6bb4fa404fcf40e2bafc9b13fc6ab958
Anton Onyshchenko <antononyshch@gmail.com>
2020-12-29 20:21:03 +0200

    add new tests

--- src/obj/Obj-Parser.ts
+++ src/obj/Obj-Parser.ts
@@ -58,8 +58,7 @@ export class OBJParser implements IFileParser
     readonly slashChar = '/';
     readonly NEWLINE_DEC = 10;
     readonly SPACE_DEC = 32;
-    private _buffInt8Array: Int8Array;
-    private _b_line_i: number = 0;
+    private _buffer: Int8Array;
     private _indexer: number = 0;
     private _iobjModel: IObjModel;
     private decoder : TextDecoder;
@@ -71,35 +70,37 @@ export class OBJParser implements IFileParser
         this._iobjModel.verticesNormals = new Array<Float32Array>();
         this._iobjModel.faces = new Array<Array<Int32Array>>();
     }
-    //#region Public Methods
+
     parse(buffer: ArrayBuffer): void {
-        this._buffInt8Array = new Int8Array(buffer);
-        //Structure token
-        let structureToken: ObjFileStructureTokens;
+        this._buffer = new Int8Array(buffer);
+
         //Index for 'end of file' detection
         let indexer: number = 0;
-        //One hole line
+        //One line
         let line: string;
         //First two symbols(without spaces) of line
         let tokenSymbols: string;
+        let newLineIndex = 0;
         let stringArray = [];
         let faceLineArray: Array<any> = [];
-        while(indexer <= this._buffInt8Array.byteLength)
+        while(indexer <= this._buffer.byteLength)
         {
-            //line = this.getLine(this._buffInt8Array, indexer);
-            this.findBufferNewLine();
-            line = this.decoder.decode(this._buffInt8Array.subarray(this._indexer, this._b_line_i));
+            //line = this.getLine(this._buffer, indexer);
+            newLineIndex = this.findNewLineIndex(indexer, this._buffer);
+            //string line
+            line = this.decoder.decode(this._buffer.subarray(this._indexer, newLineIndex));
+            //increment indexer
             indexer += line.length + 1;
             this._indexer = indexer;
             
+            //get token. For example: vt
             tokenSymbols = this.getTokenSymbols(line);
+            //get line without token
             line = line.slice(2, line.length);
             switch (tokenSymbols) {
                 case StructureSymbols.GeometricVertices:
                 {
-                    //Set token
-                    structureToken = ObjFileStructureTokens.GeometricVertices;
-
+                    //remove all spaces at the beginning
                     line = this.deleteFirstSpaces(line);
 
                     if(line.includes(this.doubleSpaceChar)) {
@@ -164,6 +165,7 @@ export class OBJParser implements IFileParser
             }
         }
     }
+
     getTokenSymbols(line: string)
     {
         let startIndex = 0;
@@ -182,15 +184,16 @@ export class OBJParser implements IFileParser
         }
         return line.slice(startIndex, i);
     }
+
     getBufferTokenSymbols()
     {
         let firstSymbolIndex = -1;
         let i = this._indexer;
-        for (; i < this._buffInt8Array.length; i++) {
-            if(this._buffInt8Array[i] === this.SPACE_DEC) {
+        for (; i < this._buffer.length; i++) {
+            if(this._buffer[i] === this.SPACE_DEC) {
                 if(firstSymbolIndex !== -1)
                 {
-                    return this._buffInt8Array.subarray(firstSymbolIndex, i);
+                    return this._buffer.subarray(firstSymbolIndex, i);
                 }
                 else
                 {
@@ -206,33 +209,27 @@ export class OBJParser implements IFileParser
         {
             firstSymbolIndex++;
         }
-        return this._buffInt8Array.subarray(firstSymbolIndex, i);
+        return this._buffer.subarray(firstSymbolIndex, i);
     }
     
-    findBufferNewLine()
+
+    findNewLineIndex(initial: number, buffer: Int8Array)
     {
-        this._b_line_i = this._indexer;
-        for (; this._b_line_i < this._buffInt8Array.byteLength; this._b_line_i++)
-        {
-            if(this._buffInt8Array[this._b_line_i] === this.NEWLINE_DEC)
-            {
-                break;
-            }
-        }
-        // if(this._b_line_i - this._indexer > this._maxLineLength)
-        // {
-        //     this._b_line_i = 0;
-        // }
+        for (let i = initial; i < buffer.length; i++)
+            if(buffer[i] === this.NEWLINE_DEC)
+                return i;
     }
-    getLine(buffer: Int8Array, startWith: number, maxLineLength = 30) {
-        let i = startWith;
-        for (; i < buffer.byteLength; i++) {
-            if(buffer[i] === 10) {
-                return this.decoder.decode(buffer.subarray(startWith, i));
-            }
-        }
-        return (i - startWith) > maxLineLength ? '' : this.decoder.decode(buffer.subarray(startWith, i));
-    }
+
+    // getLine(buffer: Int8Array, startWith: number, maxLineLength = 30) {
+    //     let i = startWith;
+    //     for (; i < buffer.byteLength; i++) {
+    //         if(buffer[i] === 10) {
+    //             return this.decoder.decode(buffer.subarray(startWith, i));
+    //         }
+    //     }
+    //     return (i - startWith) > maxLineLength ? '' : this.decoder.decode(buffer.subarray(startWith, i));
+    // }
+
     deleteFirstSpaces(line: string) {
         let nSpaces = 0;
         for (let i = 0; i < line.length; i++)
@@ -252,6 +249,7 @@ export class OBJParser implements IFileParser
             }
         }
     }
+
     split(line: string, symbol: string, limit: number) {
         const retValue = new Array();
         let symbolStart = 0;
